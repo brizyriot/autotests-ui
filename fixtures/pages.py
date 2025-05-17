1a0162d46c3f13d590b06149b7c80f92148336be
Виктория <brizyriot@yandex.ru>
2025-05-17 16:14:13 +0300

    Практикуемся в реализации PageObject. Практикуемся в написании автотестов с использованием POM

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -2,7 +2,7 @@ import pytest
 from playwright.sync_api import Page
 
 from pages.courses_list_page import CoursesListPage
-from pages.create_courses_page import CreateCoursesPage
+from pages.create_courses_page import CreateCoursePage
 from pages.dashboard_page import DashboardPage
 from pages.login_page import LoginPage
 from pages.registration_page import RegistrationPage
@@ -25,5 +25,5 @@ def courses_list_page(chromium_page_with_state: Page) -> CoursesListPage:
     return CoursesListPage(page=chromium_page_with_state)
 
 @pytest.fixture
-def create_course_page(chromium_page_with_state: Page) -> CreateCoursesPage:
-    return CreateCoursesPage(page=chromium_page_with_state)
+def create_course_page(chromium_page_with_state: Page) -> CreateCoursePage:
+    return CreateCoursePage(page=chromium_page_with_state)
--- pages/create_courses_page.py
+++ pages/create_courses_page.py
@@ -2,7 +2,7 @@ from playwright.sync_api import Page, expect
 
 from  pages.base_page import BasePage
 
-class CreateCoursesPage(BasePage):
+class CreateCoursePage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
