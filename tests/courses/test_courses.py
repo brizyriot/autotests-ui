ae98ff794579161ef92ebd53dc7292ac8236f9a3
Виктория <brizyriot@yandex.ru>
2025-05-21 19:20:49 +0300

    Практикуемся в написании автотестов с использованием Page Object, Page Component, Page Factory

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -33,3 +33,21 @@ class TestCourses:
         courses_list_page.course_view.check_visible(
             index=0, title="Playwright", max_score="100", min_score="10", estimated_time="2 weeks"
         )
+
+    def test_edit_course(self, create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
+        create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
+        create_course_page.create_course_form.fill(title="Playwright", estimated_time="2 weeks",
+                                                   description="Playwright",
+                                                   max_score="100", min_score="10")
+        create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
+        create_course_page.create_course_toolbar.click_create_course_button()
+        courses_list_page.course_view.check_visible(
+            index=0, title="Playwright", max_score="100", min_score="10", estimated_time="2 weeks"
+        )
+        courses_list_page.course_view.menu.click_edit(index=0)
+        create_course_page.create_course_form.fill(title="Python", estimated_time="5 weeks",
+                                                   description="Python",
+                                                   max_score="200", min_score="20")
+        create_course_page.create_course_toolbar.click_create_course_button()
+        courses_list_page.course_view.check_visible(
+            index=0, title="Python", max_score="200", min_score="20", estimated_time="5 weeks")
