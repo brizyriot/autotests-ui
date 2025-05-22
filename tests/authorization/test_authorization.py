77b6b09400ff62d87ea30c7a019b74c65ea831d6
Виктория <brizyriot@yandex.ru>
2025-05-22 20:29:32 +0300

    Интеграция Allure в автотесты

--- tests/authorization/test_authorization.py
+++ tests/authorization/test_authorization.py
@@ -1,12 +1,26 @@
 import pytest
+import allure
+from allure_commons.types import Severity
+
 from pages.authentication.login_page import LoginPage
 from pages.authentication.registration_page import RegistrationPage
 from pages.dashboard.dashboard_page import DashboardPage
+from tools.allure.tags import AllureTag
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
 
 
 @pytest.mark.regression
 @pytest.mark.authorization
+@allure.tag(AllureTag.REGRESSION, AllureTag.AUTHORIZATION)
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.AUTHENTICATION)
+@allure.story(AllureStory.AUTHORIZATION)
 class TestAuthorization:
+    @allure.tag(AllureTag.USER_LOGIN)
+    @allure.severity(Severity.BLOCKER)
+    @allure.title("User login with correct email and password")
     def test_successful_authorization(
             self,
             login_page: LoginPage,
@@ -37,6 +51,9 @@ class TestAuthorization:
             ("  ", "password")
         ]
     )
+    @allure.tag(AllureTag.USER_LOGIN)
+    @allure.severity(Severity.CRITICAL)
+    @allure.title("User login with wrong email or password")
     def test_wrong_email_or_password_authorization(self, login_page: LoginPage, email: str, password: str):
         login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
         login_page.login_form.fill(email=email, password=password)
@@ -44,6 +61,9 @@ class TestAuthorization:
         login_page.click_login_button()
         login_page.check_visible_wrong_email_or_password_alert()
 
+    @allure.tag(AllureTag.NAVIGATION)
+    @allure.severity(Severity.NORMAL)
+    @allure.title("Navigation from login page to registration page")
     def test_navigate_from_authorization_to_registration(
             self,
             login_page: LoginPage,
