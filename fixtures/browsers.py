095f8189d6384a1a14c5411e4881f92fb2977ad0
Виктория <brizyriot@yandex.ru>
2025-05-24 13:30:50 +0300

    Настройки автотестов с Pydantic.Практика работы с настройками

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -4,7 +4,6 @@ from playwright.sync_api import Playwright, Page
 
 from pages.authentication.registration_page import RegistrationPage
 from _pytest.fixtures import SubRequest
-
 from tools.playwright.pages import initialize_playwright_page
 from config import settings
 
@@ -17,11 +16,11 @@ def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
 @pytest.fixture(scope="session")
 def initialize_browser_state(playwright: Playwright):
     browser = playwright.chromium.launch(headless=True)
-    context = browser.new_context()
+    context = browser.new_context(base_url=settings.get_base_url())
     page = context.new_page()
 
     registration_page = RegistrationPage(page=page)
-    registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    registration_page.visit("./#/auth/registration")
     registration_page.registration_form.fill(email=settings.test_user.email,
                                              username=settings.test_user.username,
                                              password=settings.test_user.password)
@@ -33,4 +32,7 @@ def initialize_browser_state(playwright: Playwright):
 
 @pytest.fixture
 def chromium_page_with_state(request: SubRequest, initialize_browser_state, playwright: Playwright) -> Page:
-    yield from initialize_playwright_page(playwright, test_name=request.node.name, storage_state=settings.browser_state_file)
+    yield from initialize_playwright_page(playwright,
+                                          test_name=request.node.name,
+                                          storage_state=settings.browser_state_file
+                                          )
